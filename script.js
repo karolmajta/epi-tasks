a6b7e27c54d73ab79e5e28669f793d61691924ca
Karol Majta <karol+epi@karolmajta.com>
2014-03-02 18:34:58 +0100

    Added binary search in array example

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 (function () {
   'use strict';
 
-  var myArray = [1, 8, 12, 2, 33, 4, 9, 10, 22, 8, 12, 128, -2, 33];
-  var numberToLookFor = 12;
-  var atIndex = null; // we didnt find the number yet
-  for (var i = 0; i < myArray.length; i++) {
-    if (myArray[i] == numberToLookFor) {
-      atIndex = i;
-      console.log('Found at index: ' + atIndex);
-      break;
+  var myArray = [-3, -1, 1, 8, 12, 18, 33, 44, 51, 53, 54, 80, 129, 140, 1029, 2033];
+  var numberToLookFor = 18;
+  var leftIndex = 0;
+  var rightIndex = myArray.length-1;
+  var i = 0;
+  while(rightIndex - leftIndex > 1) {
+    var pivotPoint = leftIndex + Math.floor((rightIndex - leftIndex) / 2);
+    if (numberToLookFor > myArray[pivotPoint]) {
+      leftIndex = pivotPoint;
+    } else {
+      rightIndex = pivotPoint;
     }
   }
-  if (atIndex === null) {
+  if (myArray[leftIndex] == numberToLookFor) {
+    console.log('Found at index: ' + leftIndex);
+  } else if (myArray[rightIndex] == numberToLookFor) {
+    console.log('Found at index: ' + rightIndex);
+  } else {
     console.log('Not found');
   }
 
